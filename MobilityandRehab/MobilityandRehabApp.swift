d5899564be7d7e2065b4766cb3898544fa8f61b2
Jhhs xcode <phamthaoquyen@gmail.com>
2023-12-22 08:58:34 -0600

    Initial Commit

---
+++ MobilityandRehab/MobilityandRehabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilityandRehabApp.swift
+//  MobilityandRehab
+//
+//  Created by Jenny Pham on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MobilityandRehabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
