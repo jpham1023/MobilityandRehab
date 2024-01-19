1e607b36d6e67d9c1a781304e69af2eac449cbb0
Marcus Lustria <mlustria6238@stu.d214.org>
2024-01-19 11:17:15 -0600

    Changed the file names and  changed variable names. Changed the array names to less confusing names.

--- MobilityandRehab/RehabViewModel.swift
+++ MobilityandRehab/RehabViewModel.swift
@@ -9,17 +9,17 @@ import Foundation
 import SwiftUI
 
 class viewmodel: ObservableObject{
-    @Published  var RegionArray:[RegionArea] = [
+    @Published  var JointArray:[RegionArea] = [
     RegionArea(Joint: "Back", Regions: "Shoulder"),
     RegionArea(Joint: "triceps", Regions: "Shoulder"),
     RegionArea(Joint: "Calves", Regions: "Legs")
     ]
     
-    @Published var exeriseArray: [Exersise] = [
-    Exersise(joint: "Back", Exersise: "Extentions", video: "www.yt.com", notes: "notes"),
-    Exersise(joint: "Back", Exersise: "Raises", video: "www.youtube.com", notes: "Dont cave shoulders"),
-    Exersise(joint: "triceps", Exersise: "pullups", video: "www.yt.com", notes: "none"),
-    Exersise(joint: "Calves", Exersise: "calf raises", video: "www.yt.com", notes: "no notes")
+    @Published var ExerciseArray: [Exercise] = [
+    Exercise(joint: "Back", Exercise: "Extentions", video: "www.yt.com", notes: "notes"),
+    Exercise(joint: "Back", Exercise: "Raises", video: "www.youtube.com", notes: "Dont cave shoulders"),
+    Exercise(joint: "triceps", Exercise: "pullups", video: "www.yt.com", notes: "none"),
+    Exercise(joint: "Calves", Exercise: "calf raises", video: "www.yt.com", notes: "no notes")
     
     ]
     
