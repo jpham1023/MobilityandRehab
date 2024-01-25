1213cc27c4da0e10042a41f7debf7477eb67a6a5
Jenny Pham <qpham6326@stu.d214.org>
2024-01-25 11:13:59 -0600

    Wrote code to pull exercise data from firebase

--- MobilityandRehab/RehabViewModel.swift
+++ MobilityandRehab/RehabViewModel.swift
@@ -34,13 +34,22 @@ class viewmodel: ObservableObject{
         databaseref.getData { myError,myDataSnapshot in
         var tempJointArray: [jointType] = []
         var tempExerciseArray: [Exercise] = []
-        print(myDataSnapshot)
+        //print(myDataSnapshot)
         for region in myDataSnapshot?.children.allObjects as! [DataSnapshot]{
             let regionName = region.key
             guard let jointdictionary = region.value as? [String:Any] else {return}
-            for jointNames in jointdictionary.keys{
-                guard let exerciseDictionary = jointdictionary[jointNames] as? [String:Any] else {return}
+            print(jointdictionary)
+            for jointName in jointdictionary.keys{
+                guard let exerciseDictionary = jointdictionary[jointName] as? [String:Any] else {return}
                 print(exerciseDictionary)
+                for exercises in exerciseDictionary{
+                    guard let exerciseName = exercises.key as? String else{return}
+                    guard let linkDictionary = exercises.value as? [String:Any] else{return}
+                    guard let link = linkDictionary["link"] else{return}
+                    guard let notes = linkDictionary["Note"] else{return}
+                    let currentExercise = Exercise(joint: jointName, Exercise: exerciseName, video: link as! String, notes: notes as! String)
+                    tempExerciseArray.append(currentExercise)
+                }
             }
                     
             }
