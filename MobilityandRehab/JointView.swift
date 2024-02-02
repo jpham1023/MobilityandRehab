dbd7ce210fe88bc7a45d200e5b8e2661cfb4f09b
Dylan Domeracki <domerackidylan@gmail.com>
2024-02-02 10:34:44 -0600

    Created Joint View

---
+++ MobilityandRehab/JointView.swift
@@ -0,0 +1,16 @@
+//
+//  JointView.swift
+//  MobilityandRehab
+//
+//  Created by Dylan Domeracki on 2/2/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct JointView: View{
+    var body: some View{
+        Text("Hi")
+        
+    }
+}
