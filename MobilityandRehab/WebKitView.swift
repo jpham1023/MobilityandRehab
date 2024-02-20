d20728511f0be4a4af50c071fa997ddcdcad8cb1
Kazuya Nishizaka <knishizaka6294@stu.d214.org>
2024-02-20 10:47:36 -0600

    Added videoID

--- MobilityandRehab/WebKitView.swift
+++ MobilityandRehab/WebKitView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable{
+    let videoID: String
     
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let youtubeURL = "https://www.youtube.com/embed/\(videoID)?playsinline=1"
+        if let url = URL(string: youtubeURL){
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
 }
 
 
 
 
-let videoID: String
