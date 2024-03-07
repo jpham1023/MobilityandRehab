8ed93ddde5778e858879d5180950bd981a3be70c
Jenny Pham <qpham6326@stu.d214.org>
2024-03-07 10:54:22 -0600

    copied code from content view into regionList

--- MobilityandRehab/ContentView.swift
+++ MobilityandRehab/ContentView.swift
@@ -8,27 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var viewobject:RehabViewmodel
+    
     var body: some View {
-        Text("Regions")
-        let nameArray = viewobject.regionArray
-        NavigationStack{
-            List{
-                ForEach(nameArray,id:\.self){
-                    currentRegion in
-                    HStack{
-                        NavigationLink(destination: JointView(currentRegion: currentRegion)){
-                            Text(currentRegion)
-                                .font(.system(size:100))
-                                .font(.title)
-                            }
-                        .frame(height:150)
-                    }
-                }
-            }
-            
-        }
-    }
 }
 
 #Preview {
--- MobilityandRehab/regionList.swift
+++ MobilityandRehab/regionList.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+struct regionList: View{
+    @EnvironmentObject var viewobject:RehabViewmodel
+    var body: some View{
+        {
+            Text("Regions")
+            let nameArray = viewobject.regionArray
+            NavigationStack{
+                List{
+                    ForEach(nameArray,id:\.self){
+                        currentRegion in
+                        HStack{
+                            NavigationLink(destination: JointView(currentRegion: currentRegion)){
+                                Text(currentRegion)
+                                    .font(.system(size:100))
+                                    .font(.title)
+                                }
+                            .frame(height:150)
+                        }
+                    }
+                }
+                
+            }
+        }
+    }
+}
