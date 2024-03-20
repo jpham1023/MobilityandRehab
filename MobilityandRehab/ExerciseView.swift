422bf77741c68fd6241905319b94e438332ab5e4
Kazuya Nishizaka <knishizaka6294@stu.d214.org>
2024-03-20 11:18:54 -0500

    changed frame of video to make it look better

--- MobilityandRehab/ExerciseView.swift
+++ MobilityandRehab/ExerciseView.swift
@@ -27,7 +27,7 @@ struct ExerciseView: View {
                             HStack{
                                 Spacer()
                                 WebView(videoID: Currentexercise.videoId)
-                                    .frame(width: 375, height: 600)
+                                    .frame(width: 900, height: 562.5)
                                 Spacer()
                             }
                             Text(Currentexercise.notes)
