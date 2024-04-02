75ad5101f7af13400b7bc5dec51c336ab2bcc55b
Teodor Djuric <tdjuric5081@stu.d214.org>
2024-04-02 11:20:36 -0500

    Add grey background

--- MobilityandRehab/ContentView.swift
+++ MobilityandRehab/ContentView.swift
@@ -30,6 +30,9 @@ struct ContentView: View {
                 }
             }
             
+        }.background {
+            Color(.gray)
+                .ignoresSafeArea()
         }
     }
 }
