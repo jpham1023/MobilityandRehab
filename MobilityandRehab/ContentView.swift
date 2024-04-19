0ad2775a4f16bb9b76866e76fbf15fd23e35842b
Marcus Lustria <mlustria6238@stu.d214.org>
2024-04-19 11:18:32 -0500

    added button for homepage

--- MobilityandRehab/ContentView.swift
+++ MobilityandRehab/ContentView.swift
@@ -9,31 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            Text("Rehab Me")
-             
-                .font(.system(size: 65))
-            
-            Image("HerseyLogo")
-                .resizable()
-                .scaledToFit()
-                .frame(height:400)
-                .padding()
-            NavigationLink(destination:regionList()){
-                ZStack{
-                    Rectangle()
-                        .foregroundStyle(Color(red: 253/255, green: 102/255, blue: 26/255))
-                        .frame(width:650, height:50)
-                    Text("Lets Go")
-                        .font(.system(size:35))
-                        .frame(width:600,height:50)
-                        .foregroundColor(.white)
+        VStack {
+            NavigationStack{
+                Text("Rehab Me")
+                
+                    .font(.system(size: 65))
+                
+                Image("HerseyLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:400)
+                    .padding()
+                NavigationLink(destination:regionList()){
+                    ZStack{
+                        
+                        Rectangle()
+                            .foregroundStyle(Color(red: 253/255, green: 102/255, blue: 26/255))
+                            .frame(width:650, height:50)
+                        Text("Sign In")
+                            .font(.system(size:35))
+                            .frame(width:650,height:50)
+                            .foregroundColor(.white)
+                    }
+                    NavigationLink(destination:regionList()){
+                        ZStack {
+                            Rectangle()
+                                .foregroundStyle(Color(red: 253/255, green: 102/255, blue: 26/255))
+                                .frame(width:650, height:200)
+                            Text("Lets Go")
+                                .font(.system(size:35))
+                                .frame(width:600,height:50)
+                                .foregroundColor(.white)
+                            // NavigationLink(destination:regionList()){
+                            
+                        }
+                        
+                    }.background {
+                        Color(.gray)
+                            .ignoresSafeArea()
+                    }
                 }
             }
-            
-        }.background {
-            Color(.gray)
-                .ignoresSafeArea()
         }
     }
 }
