7a5b029bd79176ec477b3667290915f2659bebd2
Marcus Lustria <mlustria6238@stu.d214.org>
2024-05-28 10:17:21 -0500

    Added shadow to all the images and texts in homepage

--- MobilityandRehab/ContentView.swift
+++ MobilityandRehab/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
                         .foregroundStyle(Color(red: 253/255, green: 102/255, blue: 26/255))
                         .font(.system(size: 80))
                         .frame(height:100)
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                        .shadow(color: .white.opacity(0.7), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
                     Image("HerseyLogo")
                         .resizable()
                         .scaledToFit()
@@ -24,12 +26,16 @@ struct ContentView: View {
                         .cornerRadius(10)
                         .offset(y:35)
                         .padding()
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                        .shadow(color: .white.opacity(0.7), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
                     Text("Believe you can and you are already halfway there \n -Theodore Roosevelt")
                         .font(.system(size:30))
                         .bold()
                         .multilineTextAlignment(.center)
                         .foregroundStyle(Color(red: 253/255, green: 102/255, blue: 26/255))
                         .offset(y:45)
+                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 10, y: 10)
+                        .shadow(color: .white.opacity(0.7), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -5, y: -5)
                 }
                 
             }
